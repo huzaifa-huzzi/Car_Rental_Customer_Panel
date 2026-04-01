a743eca06d0aaaa3b48b9e9e6c54eb3d37582fed
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2026-04-01 16:44:43 +0500

    Gradle things got done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.car_rental_customerpanel"
+    namespace = "com.softsnip.carrental.customer"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.car_rental_customerpanel"
+        applicationId = "com.softsnip.carrental.customer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
